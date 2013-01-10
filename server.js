9f215dc48be825de90574ea512d054f8b7727e1d
Dave Turissini <davidairborne@gmail.com>
2013-01-09 22:40:42 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.configure(function () {
+	app.use('/media', express.static(__dirname + '/media'));
+  	app.use(express.static(__dirname + '/public'));
+})
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
