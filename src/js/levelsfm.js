8ad90f55c8fe76c9aff14a0395a81ad4cf2e1a9a
Dave Turissini <davidairborne@gmail.com>
2014-04-01 00:01:49 -0700

    Backend path

--- src/js/levelsfm.js
+++ src/js/levelsfm.js
@@ -31700,7 +31700,7 @@ stateless
 }).call(this,require("/Users/davidturissini/Sites/levels-fm/node_modules/grunt-browserify/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js"),"/")
 },{"./model/Station":121,"./model/User":124,"./ui/user/UserNameLabel":141,"./views/user/Login":145,"./views/user/Radio":146,"/Users/davidturissini/Sites/levels-fm/node_modules/grunt-browserify/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js":19,"backbone":1,"jquery":37,"q":60,"stateless":107}],126:[function(require,module,exports){
 var pigeon = require('pigeon');
-var domain = 'http://localhost:3000'; //*/'http://levelsfm-backend.herokuapp.com';
+var domain = /*'http://localhost:3000'; //*/'http://levelsfm-backend.herokuapp.com';
 
 var fetch = exports.get = function (path, params, method) {
 	method = method || 'get';
